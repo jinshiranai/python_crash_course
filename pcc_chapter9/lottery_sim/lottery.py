731e64a57c0cea3be893baabcc3875a91b381b3e
jinshiranai <jin.shiranai@gmail.com>
2020-06-27 22:01:08 +0900

    Add AutoLottery child class to the lottery.py module

--- pcc_chapter9/lottery_sim/lottery.py
+++ pcc_chapter9/lottery_sim/lottery.py
@@ -22,5 +22,69 @@ class Lottery:
         
         return sorted(lottery_choice)
 
-lottery = Lottery()
-print(lottery.lottery_selection())
+
+class AutoLottery(Lottery):
+    """Simulates a lottery until jackpot with various ticket conditions."""
+
+    def __init__(self):
+        """Initialize attributes of Lottery."""
+        super().__init__()
+    
+    def single_ticket_lottery(self):
+        """Simulates playing the lottery with a set ticket."""
+        ticket_numbers = self.lottery_selection()
+        winning_numbers = ''
+        draw_count = 0
+
+        print(f"\nYour numbers are: {ticket_numbers}")
+
+        while winning_numbers != ticket_numbers:
+            draw_count += 1
+            winning_numbers = self.lottery_selection()
+
+        print(f"It took {draw_count} draws to win the lottery.")
+        print(f"The winning ticket was: {winning_numbers}")
+
+    def random_ticket_lottery(self):
+        """Simulates playing the lottery with a random ticket every draw."""
+        ticket_numbers = ' '
+        winning_numbers = ''
+        draw_count = 0
+
+        while winning_numbers != ticket_numbers:
+            draw_count += 1
+            winning_numbers = self.lottery_selection()
+            ticket_numbers = self.lottery_selection()
+
+        print(f"It took {draw_count} draws to win the lottery.")
+        print(f"The winning ticket was: {winning_numbers}")
+
+    def lucky_numbers_ticket_lottery(self):
+        """
+        Simulates playing the lottery with a single ticket of
+        user-chosen 'lucky numbers'.
+        """
+
+        ticket_numbers = []
+        winning_numbers = ''
+        draw_count = 0
+
+        while len(ticket_numbers) < 5:
+            chosen_number = input("\nPlease enter your number: ")
+            try:
+                chosen_number = int(chosen_number)
+            except ValueError:
+                print("Only numbers, please.")
+            else:
+                ticket_numbers.append(chosen_number)
+                ticket_numbers = sorted(ticket_numbers)
+
+        while winning_numbers != ticket_numbers:
+            draw_count += 1
+            winning_numbers = self.lottery_selection()
+
+        print(f"It took {draw_count} draws to win the lottery.")
+
+
+lottery = AutoLottery()
+lottery.lucky_numbers_ticket_lottery()
