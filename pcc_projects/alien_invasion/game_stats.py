e3c17fd8787a060d8a6812d5ac0319ba380335da
jinshiranai <jin.shiranai@gmail.com>
2020-07-30 23:22:11 +0900

    Final (for now) update for Alien Invasion project

--- pcc_projects/alien_invasion/game_stats.py
+++ pcc_projects/alien_invasion/game_stats.py
@@ -12,7 +12,7 @@ class GameStats:
         self.game_active = False
 
         # High score should never be reset.
-        self.high_score = 0
+        self.high_score = self.load_high_score()
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
@@ -25,6 +25,14 @@ class GameStats:
         filename = "high_score.json"
         try:
             with open(filename) as f:
-                self.high_score = int(f.read())
+                old_high_score = f.read()
         except FileNotFoundError:
             return 0
+        else:
+            return int(old_high_score)
+
+    def save_high_score(self):
+        """Save the high score on exit."""
+        filename = "high_score.json"
+        with open(filename, 'w') as f:
+            f.write(str(self.high_score))
